dfdacb9d93292a95cbf4d8978b32237a842beae9
Adam Daniels <adam@mediadrive.ca>
2024-04-10 21:15:54 -0400

    Gem init

---
+++ lib/rbz/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rbz
+  VERSION = "0.1.0"
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "rbz"
+
+require "minitest/autorun"
