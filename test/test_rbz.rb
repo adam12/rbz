8fc9296db75e7d2ee0809104b54ee4d3a9983d6c
Adam Daniels <adam@mediadrive.ca>
2024-04-12 12:05:29 -0400

    Fix test

--- test/test_rbz.rb
+++ test/test_rbz.rb
@@ -3,11 +3,18 @@
 require "test_helper"
 
 class TestRbz < Minitest::Test
+  ROOT = File.expand_path("..", __dir__)
+  RUBY = RbConfig.ruby
+
   def test_that_it_has_a_version_number
     refute_nil ::Rbz::VERSION
   end
 
   def test_it_does_something_useful
-    assert false
+    output = Dir.chdir(ROOT) do
+      `#{RUBY} -Ilib exe/rbz example | #{RUBY} - ohai`
+    end
+
+    assert_equal "Hello world\n", output
   end
 end
